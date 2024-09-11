304eba32ab8f0594af97837809b450e5a3d43001
Alan Maizon <58921943+alanmaizon@users.noreply.github.com>
2024-09-11 22:42:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,9 +12,8 @@ if sys.platform == 'win32':
     install_requires.append('pywin32==306')
 
 setup(
-    name='flask-chat-app',
+    name='glowing-five',
     version='1.0',
-    install_requires=install_requires,
-    # other setup options...
+    install_requires=install_requires
 )
 
